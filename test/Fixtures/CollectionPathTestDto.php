2f54bef9632854ac31f4b07785635d113871a609
crtl <mp@borntocreate.de>
2026-02-02 19:37:44 +0100

    fix: violation property path prefixing for array sequences. (#15)

---
+++ test/Fixtures/CollectionPathTestDto.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of a private project.
+ *
+ * Copyright 2026 Crtl
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Crtl\RequestDtoResolverBundle\Test\Fixtures;
+
+use Crtl\RequestDtoResolverBundle\Attribute\BodyParam;
+use Crtl\RequestDtoResolverBundle\Attribute\RequestDto;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[RequestDto]
+class CollectionPathTestDto
+{
+    /**
+     * @var array<int, array{key: string, value: string}>
+     */
+    #[BodyParam, Assert\NotBlank, Assert\Type('array'), Assert\All([
+        new Assert\Type('array'),
+        new Assert\Collection(fields: [
+            'key' => [
+                new Assert\NotBlank(),
+            ],
+            'value' => [
+                new Assert\NotBlank(),
+            ],
+        ]),
+    ])]
+    public array $property;
+}
